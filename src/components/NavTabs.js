5fcbce95737e1767c7a563162d5d15ac6d41051b
ShaneWilmes <shanewilmes@yahoo.com>
2022-07-31 18:59:47 -0500

    styling navbar page links

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -4,53 +4,55 @@ import React from 'react';
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
+    <div class='tabs is-centered has-background-grey-dark'>
     <ul>
       <li>
-        <a
-          class= "is-active "
+        <a class="is-active has-text-light"
           href="#aboutMe"
           onClick={() => handlePageChange('AboutMe')}
-          
+
 
           className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
         >
           About Me
         </a>
       </li>
-      <li className="nav-item">
-        <a
+      <li class="nav-item">
+        <a class="is-active has-text-light"
           href="#portfolio"
           onClick={() => handlePageChange('Portfolio')}
-          
+
 
           className={currentPage === 'portfolio' ? 'nav-link active' : 'nav-link'}
         >
           Portfolio
         </a>
       </li>
-      <li className="nav-item">
-        <a
+      <li class="nav-item">
+        <a class="is-active has-text-light"
           href="#resume"
           onClick={() => handlePageChange('Resume')}
-          
+
 
           className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
         >
           Resume
         </a>
       </li>
-      <li className="nav-item">
-        <a
+      <li class="nav-item">
+        <a class="is-active has-text-light"
           href="#contact"
-          
+
 
           onClick={() => handlePageChange('Communicate')}
+
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
         >
           Contact Info
         </a>
       </li>
     </ul>
+    </div>
   );
 }
 
